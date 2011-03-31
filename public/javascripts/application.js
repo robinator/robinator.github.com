421dd3f61902ef7a9bde52cd12095a9e79c4d955
Robert Law <rob@flywheelnetworks.com>
2011-03-30 19:27:34 -0700

    Updates.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,9 @@
   $(document).ready(function() {
     var line, paper;
     paper = Raphael('paper', 900, 500);
-    return (line = paper.rect(50, 50, 10, 300).attr({
+    return line = paper.rect(50, 50, 10, 300).attr({
       stroke: 'none',
       fill: '#38d84e'
-    }));
+    });
   });
-})();
+}).call(this);
