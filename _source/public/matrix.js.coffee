848daa27c953406308e245ec058964b4423ec4e5
Rob Law <rob@flywheelnetworks.com>
2012-05-21 16:06:55 -0700

    Final fixes for matrix stuff.

--- _source/public/matrix.js.coffee
+++ _source/public/matrix.js.coffee
@@ -3,76 +3,68 @@ class Matrix
     @actualFile = 'public/data/actual.json'
     @guessesFile = 'public/data/guesses.json'
     @vis = d3.select('#matrix')
-    this.loadActual()
 
-  loadActual: ->
+  _process: =>
+    for guess in @guesses
+      guess.score = 0
+      guess.pointsPossible = 30
+      eliminated = []
+      for pick, i in guess.picks
+        actual = @actual[i]
+        eliminated.push(actual[1]) unless actual[1] == '???'
+        pick[2] = 0
+        if pick[0] == actual[0]
+          # right
+          guess.score += 1
+          pick[2] += 1
+          if pick[1] == actual[2]
+            # and1
+            guess.score += 1
+            pick[2] += 1
+        else if actual[0] != '???' || pick[0] in eliminated
+          # wrong
+          pick[2] -= 1
+          guess.pointsPossible -= 2
+        # console.log("#{guess.name}(#{i}) - pick: #{pick}, actual: #{actual}, score: #{guess.score} / #{guess.pointsPossible}")
+
+  draw: ->
     d3.json @actualFile, (json) =>
       @actual = json
       @maxScore = @actual.length * 2
 
-  process: ->
-    for guess in @guesses
-      guess.score = 0
-      for pick, i in guess.picks
-        actual = @actual[i]
-        if pick[0] == actual[0]
-          guess.score += 1
-          guess.score += 1 if pick[1] == actual[1]
-        console.log("#{guess.name}(#{i}) - pick: #{pick}, actual: #{actual}, score: #{guess.score} / #{@maxScore}")
+      d3.json @guessesFile, (json) =>
+        @guesses = json
+        @_process()
 
-  draw: ->
-    d3.json @guessesFile, (json) =>
-      @guesses = json
-      this.process()
+        tr = d3.select('tbody')
+               .selectAll('tr')
+               .data(@guesses)
+               .enter()
+               .append('tr')
+               # .sort(function (a, b) { return a == null || b == null ? 0 : stringCompare(a[attrName], b[attrName]); });
 
-      # // Header
-      #           var th = d3.select("thead").selectAll("th")
-      #                   .data(jsonToArray(data[0]))
-      #                 .enter().append("th")
-      #                   .attr("onclick", function (d, i) { return "transform('" + d[0] + "');";})
-      #                   .text(function(d) { return d[0]; })
-
-      console.log(@guesses)
-
-      tr = d3.select('tbody')
-             .selectAll('tr')
-             .data(@guesses)
-             .enter()
-             .append('tr')
-             # .sort(function (a, b) { return a == null || b == null ? 0 : stringCompare(a[attrName], b[attrName]); });
-
-      td = tr.selectAll('td')
-             .data((d) ->
-               arr = d.picks
-               arr.unshift(d.score)
-               arr.unshift(d.name)
-               arr
-             )
-             .enter()
-             .append('td')
-             .classed('wrong', (d) -> typeof d != 'string' && false)
-             .classed('right', (d) -> typeof d != 'string' && false)
-             .classed('and1', (d) -> typeof d != 'string' && false)
-             .classed('name', (d) -> typeof d == 'string')
-             .text((d) =>
-               if typeof d == 'string'
-                 d
-               else if typeof d == 'number'
-                 "#{d} / #{@maxScore}"
-               else
-                 "#{d[0]} in #{d[1]}"
-             )
-             #.attr("onclick", function (d, i) { return "transform('" + d[0] + "');";})
-
-      # for name, picks in @data
-      #        console.log(name)
-      #        @vis.select('table')
-      #            .selectAll('tr')
-      #            .enter()
-      #            .append('tr')
-      #            .text('poop')
+        td = tr.selectAll('td')
+               .data((d) ->
+                 arr = d.picks
+                 arr.unshift(d.pointsPossible)
+                 arr.unshift(d.score)
+                 arr.unshift(d.name)
+                 arr
+               )
+               .enter()
+               .append('td')
+               .classed('wrong', (d) -> typeof d == 'object' && d[2] == -1)
+               .classed('right', (d) -> typeof d == 'object' && d[2] == 1)
+               .classed('and1', (d) -> typeof d == 'object' && d[2] == 2)
+               .classed('name', (d) -> typeof d == 'string')
+               .text((d) =>
+                 if typeof d == 'string'
+                   d
+                 else if typeof d == 'number'
+                   d
+                 else
+                   "#{d[0]} in #{d[1]}"
+               )
 
 window.matrix = new Matrix
-
-#matrix.process()
 matrix.draw()
--- public/matrix.js
+++ public/matrix.js
@@ -1,45 +1,54 @@
 (function() {
-  var Matrix;
+  var Matrix,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
   Matrix = (function() {
 
     function Matrix(options) {
       this.options = options;
+      this._process = __bind(this._process, this);
+
       this.actualFile = 'public/data/actual.json';
       this.guessesFile = 'public/data/guesses.json';
       this.vis = d3.select('#matrix');
-      this.loadActual();
     }
 
-    Matrix.prototype.loadActual = function() {
-      var _this = this;
-      return d3.json(this.actualFile, function(json) {
-        _this.actual = json;
-        return _this.maxScore = _this.actual.length * 2;
-      });
-    };
-
-    Matrix.prototype.process = function() {
-      var actual, guess, i, pick, _i, _len, _ref, _results;
+    Matrix.prototype._process = function() {
+      var actual, eliminated, guess, i, pick, _i, _len, _ref, _results;
       _ref = this.guesses;
       _results = [];
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         guess = _ref[_i];
         guess.score = 0;
+        guess.pointsPossible = 30;
+        eliminated = [];
         _results.push((function() {
-          var _j, _len1, _ref1, _results1;
+          var _j, _len1, _ref1, _ref2, _results1;
           _ref1 = guess.picks;
           _results1 = [];
           for (i = _j = 0, _len1 = _ref1.length; _j < _len1; i = ++_j) {
             pick = _ref1[i];
             actual = this.actual[i];
+            if (actual[1] !== '???') {
+              eliminated.push(actual[1]);
+            }
+            pick[2] = 0;
             if (pick[0] === actual[0]) {
               guess.score += 1;
-              if (pick[1] === actual[1]) {
+              pick[2] += 1;
+              if (pick[1] === actual[2]) {
                 guess.score += 1;
+                _results1.push(pick[2] += 1);
+              } else {
+                _results1.push(void 0);
               }
+            } else if (actual[0] !== '???' || (_ref2 = pick[0], __indexOf.call(eliminated, _ref2) >= 0)) {
+              pick[2] -= 1;
+              _results1.push(guess.pointsPossible -= 2);
+            } else {
+              _results1.push(void 0);
             }
-            _results1.push(console.log("" + guess.name + "(" + i + ") - pick: " + pick + ", actual: " + actual + ", score: " + guess.score + " / " + this.maxScore));
           }
           return _results1;
         }).call(this));
@@ -49,34 +58,38 @@
 
     Matrix.prototype.draw = function() {
       var _this = this;
-      return d3.json(this.guessesFile, function(json) {
-        var td, tr;
-        _this.guesses = json;
-        _this.process();
-        console.log(_this.guesses);
-        tr = d3.select('tbody').selectAll('tr').data(_this.guesses).enter().append('tr');
-        return td = tr.selectAll('td').data(function(d) {
-          var arr;
-          arr = d.picks;
-          arr.unshift(d.score);
-          arr.unshift(d.name);
-          return arr;
-        }).enter().append('td').classed('wrong', function(d) {
-          return typeof d !== 'string' && false;
-        }).classed('right', function(d) {
-          return typeof d !== 'string' && false;
-        }).classed('and1', function(d) {
-          return typeof d !== 'string' && false;
-        }).classed('name', function(d) {
-          return typeof d === 'string';
-        }).text(function(d) {
-          if (typeof d === 'string') {
-            return d;
-          } else if (typeof d === 'number') {
-            return "" + d + " / " + _this.maxScore;
-          } else {
-            return "" + d[0] + " in " + d[1];
-          }
+      return d3.json(this.actualFile, function(json) {
+        _this.actual = json;
+        _this.maxScore = _this.actual.length * 2;
+        return d3.json(_this.guessesFile, function(json) {
+          var td, tr;
+          _this.guesses = json;
+          _this._process();
+          tr = d3.select('tbody').selectAll('tr').data(_this.guesses).enter().append('tr');
+          return td = tr.selectAll('td').data(function(d) {
+            var arr;
+            arr = d.picks;
+            arr.unshift(d.pointsPossible);
+            arr.unshift(d.score);
+            arr.unshift(d.name);
+            return arr;
+          }).enter().append('td').classed('wrong', function(d) {
+            return typeof d === 'object' && d[2] === -1;
+          }).classed('right', function(d) {
+            return typeof d === 'object' && d[2] === 1;
+          }).classed('and1', function(d) {
+            return typeof d === 'object' && d[2] === 2;
+          }).classed('name', function(d) {
+            return typeof d === 'string';
+          }).text(function(d) {
+            if (typeof d === 'string') {
+              return d;
+            } else if (typeof d === 'number') {
+              return d;
+            } else {
+              return "" + d[0] + " in " + d[1];
+            }
+          });
         });
       });
     };
